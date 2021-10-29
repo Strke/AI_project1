754329e74aba7a4c65f21c7a3edac12fb99a7ec7
Lin <1210081098@qq.com>
2021-10-29 23:46:51 +0800

    first commit

---
+++ Config.py
@@ -0,0 +1,11 @@
+# The path to the directory which stores all the datasets.
+# If equals "", the path will set to the path to the directory which stores the datasets downloaded from Digix website.
+BASE_DATASET_PATH = "C:\\Users\\Linrenze\\Desktop\\DIGIX\\solve\\baseline-game2\\dataset"
+# The path to the directory which stores all the trained model files.
+# If equals "", the path will set to the path to the sub directory "model" at current directory
+BASE_MODEL_PATH = "C:\\Users\\Linrenze\\Desktop\\DIGIX\\solve\\baseline-game2\\model"
+# The path to the pretrained BERT ENCODER, such as "/data/bert_base_chinese", you can download it at https://huggingface.co/bert-base-chinese/tree/main
+PRETRAINED_BERT_ENCODER_PATH = "C:\\Users\\Linrenze\\Desktop\\DIGIX\\solve\\data\\bert_base_chinese"
+# The path to save the structured result(.csv) of evaluating test files.
+# If equals "", the path will set to the file "submission.csv" at the current directory
+SUMMARY_OUTPUT_PATH = ""
